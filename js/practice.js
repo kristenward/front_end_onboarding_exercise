7bcd908917b230bf1f7a7a35825246456530e9ca
Kristen Ward <kward@weblinc.com>
2013-08-14 17:55:04 -0400

    WLPROD-70: 4/5

--- js/practice.js
+++ js/practice.js
@@ -1,44 +1,50 @@
-
-//navigation
-
-
 $('.no-js').addClass('js').removeClass('no-js');
 
 $(document).ready(function(){
 
-
+	
 	$('.wl-nav li>a').click(function(){ //open & close menus
 
 			$li = $(this).parent(); 
 			$sublist = $li.children('ul.wl-sub, ul.wl-sub2');
 			$nephews = $li.siblings().find('ul.wl-sub, ul.wl-sub2');
-			$arrow = $(this).children('.arrow');
+			//$arrow = $(this).children('.arrow');
+			
+			$li.find('li').removeClass('active-cat').removeClass('active-parent').find('ul').hide();
 
-			//if($li.hasClass('active-cat'))
-			$li.find('li').removeClass('active-cat');
+				if ($li.parent('.wl-sub')) { //children of parent categories only
 
-			if ($li.parent('.wl-sub')) {//hasClass('subcat')) {  //children of parent categories only
+					if($li.hasClass('active-parent')){
+						$li.removeClass('active-parent').removeClass('active-cat').children('ul').hide();
+						//$sublist.find('ul.wl-sub, ul-wl-sub2').hide();
+					}
 
-				$parentli = $li.parent().parent();
+					$parentli = $li.parent().parent();
 
-				if (!$li.hasClass('active-cat')){ 
-					$('.wl-sub>li').removeClass('active-cat');
-					$parentli.removeClass('active-parent');
-				} 
-				$parentli.toggleClass('active-parent');
-			}
+					if (!$li.hasClass('active-cat')){ 
+						$('.wl-sub>li').removeClass('active-cat');
+						$parentli.removeClass('active-parent');
+						$sublist.toggle();
+					} else{
+						$li.find('ul').hide();
+					}
+					$parentli.toggleClass('active-parent');
+
+				} else{
+
+					//$sublist.toggle();
+				}
+
 
 			$li.toggleClass('active-cat').siblings().removeClass('active-cat').removeClass('active-parent');//.end().children('a').children('.arrow').hide();
 			$nephews.hide().parent().removeClass('active-cat');
-			$sublist.toggle();
+			
 
 	});
 
-
-	// // // // // swipe functions
+// // // // // Swipe functions
 
 	var elem = document.getElementById('PromoSwipe');
-
 	$('.swipe-wrap>div').each(function(){ $('.swipe-position').append('<li></li>');})
 	$('.swipe-position>li:first-child').addClass('on');
 
@@ -72,25 +78,23 @@ $(document).ready(function(){
 	
 	// // // // // Thumbnail functions
 
-	$('.wl-product').each(function(){
+	$('.wl-product').each(function(){ //test for swatches length
 		$swatch_list = $(this).children('.wl-product-swatches');
 		$swatches = $swatch_list.children();
 		$num_swatches = $swatches.length;
-		if ($num_swatches>8){
-			for(var i=8; i<$swatches.length; i++) {
-			$($swatches[i]).hide();
-			}
+			if ($num_swatches>8){
+				for(var i=8; i<$swatches.length; i++) {
+					$($swatches[i]).hide();
+				}
 
-			$swatch_list.append('<div class="addl">+'+($num_swatches-8));
-		}
+				$swatch_list.append('<div class="addl">+'+($num_swatches-8));
+			}
 	})
 
-	$('.wl-product-swatches>li').click(function(e){
+	$('.wl-product-swatches>li').click(function(e){ //swap large thumbs
 		e.preventDefault;
 		$prod = $(this).parent().parent();
 		$large = $prod.children('.media').find('img');
-		console.log($large);
-		//$index = $prod.index();
 		$base_href = "http://placehold.it/236x314/"
 		$color = $(this).children('img').attr('alt');
 		$large.attr('src', $base_href + $color);
